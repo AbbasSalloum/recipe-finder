20cc340c93bd906a2d127f2024a6d463acbd0357
AbbasSalloum <Salloum916@gmail.com>
2024-08-19 02:09:16 -0400

    tailwindcss init

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { store } from './redux/store';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import HomePage from './pages/HomePage';
-import './i18n';  // Import i18n for localization
+//import './i18n';  // Import i18n for localization
 
 const theme = createTheme({
     palette: {
