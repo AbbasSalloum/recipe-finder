d92dba7ac2804c1d7ec6102a160cfa193e1f7bc9
AbbasSalloum <Salloum916@gmail.com>
2024-08-19 01:57:13 -0400

    uodate to homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,23 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchRecipes } from '../redux/slices/recipeSlice';
-import SearchBar from '../components/SearchBar';
+import { useDispatch, useSelector } from 'react-redux';
+import SearchBar from './SearchBar';
+import RecipeList from './RecipeList';
+import { setRecipes } from './recipesSlice';  // Action to update recipes
+
+const HomePage = () => {
+  const dispatch = useDispatch();
+  const recipes = useSelector((state) => state.recipes.items);  // Accessing recipes from Redux store
+
+  const handleSearch = (searchResults) => {
+    dispatch(setRecipes(searchResults));  // Dispatching action to update recipes in Redux store
+  };
+
+  return (
+    <div>
+      <SearchBar onSearch={handleSearch} />
+      <RecipeList recipes={recipes} />
+    </div>
+  );
+};
+
+export default HomePage;
